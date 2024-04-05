fd7bd82846b25a7cae71a7debc4f6d6b3bcf670d
Marcos Vinicius <omarcosviniciusdev@gmail.com>
2024-04-05 17:46:34 -0300

    feat: basic types in golang

--- 01-crash_course/cmd/intro_1/main.go
+++ 01-crash_course/cmd/intro_1/main.go
@@ -1,7 +1,50 @@
 package main // Told to compiler to start here
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
-	fmt.Println("hello world")
+	var intNum int16 = 32767
+	intNum = intNum + 1 // Will overflow and return -32767
+	fmt.Println(intNum)
+
+	var floatNum float32 = 64.4
+	fmt.Println(floatNum)
+
+	var floatNum32 float32 = 10.1
+	var intNum32 int32 = 2
+	var result float32 = floatNum32 + float32(intNum32)
+	fmt.Println(result)
+
+	// Using strings
+	fmt.Println("Same line with double quotes")
+	fmt.Println(`Same text, diferent line
+	like that`)
+	fmt.Println("Concatenate" + " strings " + "using +")
+
+	//GAMMA symbol
+	// Rune is a type in golang that represente ONE character
+	// Number of bytes in a text -> use len()
+	// Number of character in a text use utf8.RuneInString()
+
+	// Booleans
+
+	fmt.Println(true)
+	fmt.Println(false)
+
+	// Default values
+
+	// Almost number and rune is 0
+	// String -> ''
+	// Boolean -> false
+
+	myVar := "text"
+	fmt.Println(myVar)
+
+	var var1, var2 int = 1, 2 // Multiple variables
+	// or var1, var2 := 1, 2
+	fmt.Println(var1, var2)
+
+	const myConst string = "const value" // Just can initialize with the values
 }
